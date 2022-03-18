9f449c4c267c1eba3d2b7c9730d04208a332f3c1
ManzanasCode <adrian.gastos@gmail.com>
2022-03-18 12:56:24 -0600

    cambios modelo olt

--- src/controllers/proxy.controller.ts
+++ src/controllers/proxy.controller.ts
@@ -2,188 +2,190 @@ import { Request, Response } from 'express'
 
 
 export async function getCredentials(req: Request, res: Response): Promise<Response | void> {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Credentials", "true")
 
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method,');
-    res.header('content-type: application/json; charset=utf-8')
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method,');
+  res.header('content-type: application/json; charset=utf-8')
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
 
-    const jsonResponse =
-    {
-        "status": "C",
-        "code": "200",
-        "response": "El servicio se ejecutó correctamente",
-        "credentials": [
-            {
-                "id": 92,
-                "anilloId": 116,
-                "userEquipo": "pdr",
-                "pwdEquipo": "Pdr1tSM#TpR3dd76",
-                "userLanWan": "pderedundancia",
-                "pwdLanwan": "321qWe...7654321",
-                "userColector": "REDUNDANCIA",
-                "pwdColector": "BMEaZIV+%4102066",
-                "userOlt": "usrpdr",
-                "pwdOlt": "SZxrtotalplay21",
-                "userUrlVideo": "mloza",
-                "pwdUrlVideo": "mloza2016",
-                "creationDate": "2021-11-18 13:11:23.0"
-            }
-        ]
-    }
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+
+  const jsonResponse =
+  {
+    "status": "C",
+    "code": "200",
+    "response": "El servicio se ejecutó correctamente",
+    "credentials": [
+      {
+        "id": 92,
+        "anilloId": 116,
+        "userEquipo": "pdr",
+        "pwdEquipo": "Pdr1tSM#TpR3dd76",
+        "userLanWan": "pderedundancia",
+        "pwdLanwan": "321qWe...7654321",
+        "userColector": "REDUNDANCIA",
+        "pwdColector": "BMEaZIV+%4102066",
+        "userOlt": "usrpdr",
+        "pwdOlt": "SZxrtotalplay21",
+        "userUrlVideo": "mloza",
+        "pwdUrlVideo": "mloza2016",
+        "creationDate": "2021-11-18 13:11:23.0"
+      }
+    ]
+  }
+
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export async function token(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(2000)
+  await sleep(2000)
 
-    const jsonResponse =
-    {"access_token":"9eacea48-ef19-4fda-b86d-883156e15771","token_type":"bearer","refresh_token":"dcf1e961-506a-465c-a08d-c815ec4cdaaa","expires_in":43199,"scope":"read write trust"}
+  const jsonResponse =
+    { "access_token": "9eacea48-ef19-4fda-b86d-883156e15771", "token_type": "bearer", "refresh_token": "dcf1e961-506a-465c-a08d-c815ec4cdaaa", "expires_in": 43199, "scope": "read write trust" }
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 
 export async function userLog(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(2000)
+  await sleep(2000)
 
-    const jsonResponse =
-    {
-        "status": "C",
-        "code": "200",
-        "response": "Exito al registrar la bitácora"
-    }
+  const jsonResponse =
+  {
+    "status": "C",
+    "code": "200",
+    "response": "Exito al registrar la bitácora"
+  }
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 
 export async function consultarRFC(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(2000)
+  await sleep(2000)
 
-    const jsonResponse =
-    {
-        "rfc": "157088",
-        "mensaje": null,
-        "success": true
-    }
+  const jsonResponse =
+  {
+    "rfc": "157088",
+    "mensaje": null,
+    "success": true
+  }
 
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 export async function evidences(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(3000)
+  await sleep(3000)
 
-    const jsonResponse =
-    {
-        "status": "C",
-        "code": "200",
-        "response": "El servicio se ejecutó correctamente"
-    }
+  const jsonResponse =
+  {
+    "status": "C",
+    "code": "200",
+    "response": "El servicio se ejecutó correctamente"
+  }
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 export async function changestatus(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(2000)
+  await sleep(2000)
 
-    const jsonResponse =
-    {
-        "status": "C",
-        "code": "200",
-        "response": "El servicio se ejecutó correctamente"
-    }
+  const jsonResponse =
+  {
+    "status": "C",
+    "code": "200",
+    "response": "El servicio se ejecutó correctamente"
+  }
 
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 export async function getIpRfc(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(2000)
+  await sleep(2000)
 
-    const jsonResponse =
-    {
-        "status": "C",
-        "code": "200",
-        "response": "El servicio se ejecutó correctamente",
-        "ipList": [
-          {
-            "anillo": null,
-            "ip": "10.253.156.38",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "10.253.156.81",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "10.253.56.46",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "10.253.188.215",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "10.253.165.99",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "10.253.157.72",
-            "equipo": null
-          },
-          {
-            "anillo": null,
-            "ip": "187.190.3.25",
-            "equipo": null
-          }
-        ]
+  const jsonResponse =
+  {
+    "status": "C",
+    "code": "200",
+    "response": "El servicio se ejecutó correctamente",
+    "ipList": [
+      {
+        "anillo": null,
+        "ip": "10.253.156.38",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "10.253.156.81",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "10.253.56.46",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "10.253.188.215",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "10.253.165.99",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "10.253.157.72",
+        "equipo": null
+      },
+      {
+        "anillo": null,
+        "ip": "187.190.3.25",
+        "equipo": null
       }
+    ]
+  }
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 
 export async function ejecutaProtocolos(req: Request, res: Response): Promise<Response | void> {
 
-    const jsonResponse = true
+  const jsonResponse = true
 
-    res.json(jsonResponse);
-    res.status(200)
-    res.end()
+  res.json(jsonResponse);
+  res.status(200)
+  res.end()
 }
 
 export async function validacionacceso(req: Request, res: Response): Promise<Response | void> {
@@ -197,151 +199,175 @@ export async function validacionacceso(req: Request, res: Response): Promise<Res
 
 export async function getPerformance(req: Request, res: Response): Promise<Response | void> {
 
-    var regreso = (Math.floor(Math.random() * 2)) == 1 ? "100": "50";
-    res.json(regreso);
-    res.status(200)
-    res.end()
+  var regreso = (Math.floor(Math.random() * 2)) == 1 ? "100" : "50";
+  res.json(regreso);
+  res.status(200)
+  res.end()
 }
 
 
 export async function datosOSPFv2(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(1000)
+  await sleep(1000)
 
-    console.log("OSPF Req: ", JSON.stringify(req.body))
-    res.json({});
-    res.status(200)
-    res.end()
+  console.log("OSPF Req: ", JSON.stringify(req.body))
+  res.json({});
+  res.status(200)
+  res.end()
 }
 
 export async function validacionLLDP(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(3000)
+  await sleep(3000)
 
 
-    res.json({
-        "success": true,
-        "timeRes": "7 s, 779 ms",
-        "error": "",
-        "mssg": "Ejecucion de LLDP correcta, anillo:ECATEPEC_CENTRO_B",
-        "total": 1,
-        "data": [
-          {
-            "anillo": "ECATEPEC_CENTRO_B",
-            "equipo": "LA-PAZ-PUE7013-X8-CA",
-            "prueba": "LLDP",
-            "outfileb64": "c3BhQ0E+",
-            "error_level": "0",
-            "interfaz": null,
-            "desc": null,
-            "ipolt": null
-          }
-        ],
+  res.json({
+    "success": true,
+    "timeRes": "7 s, 779 ms",
+    "error": "",
+    "mssg": "Ejecucion de LLDP correcta, anillo:ECATEPEC_CENTRO_B",
+    "total": 1,
+    "data": [
+      {
+        "anillo": "ECATEPEC_CENTRO_B",
+        "equipo": "LA-PAZ-PUE7013-X8-CA",
         "prueba": "LLDP",
-        "conCortes": 0,
-        "executionSkipped": false
-      });
-    res.status(200)
-    res.end()
+        "outfileb64": "c3BhQ0E+",
+        "error_level": "0",
+        "interfaz": null,
+        "desc": null,
+        "ipolt": null
+      }
+    ],
+    "prueba": "LLDP",
+    "conCortes": 0,
+    "executionSkipped": false
+  });
+  res.status(200)
+  res.end()
 }
 
 export async function validateVRRP(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(3000)
+  await sleep(3000)
 
-    console.log("validateVRRP: ", req.body)
-    res.json({
-        "respuestaBoolean": true,
-        "codigoRespuesta": "000",
-        "descripcionRespuesta": "La interfaz 7/0/3.4085 se encontró en el dispositivo 10.180.7.3",
-        "mensajeServicio": "No_Message_Found",
-        "fechaSolicitud": "2021-12-01 10:13:59",
-        "fechaRespuesta": "2021-12-01 10:14:01",
-        "ip": "10.180.7.3",
-        "idEjecutor": "pdr",
-        "interfaceName": "7/0/3.4085",
-        "totalSesiones": 0,
-        "sesionesEstablecidas": 0,
-        "totalPeers": 0,
-        "peerFull": 0,
-        "version": null,
-        "outfileb64": "c3Bhd2Qo=",
-        "state": "Master"
-      });
-    res.status(200)
-    res.end()
+  console.log("validateVRRP: ", req.body)
+  res.json({
+    "respuestaBoolean": true,
+    "codigoRespuesta": "000",
+    "descripcionRespuesta": "La interfaz 7/0/3.4085 se encontró en el dispositivo 10.180.7.3",
+    "mensajeServicio": "No_Message_Found",
+    "fechaSolicitud": "2021-12-01 10:13:59",
+    "fechaRespuesta": "2021-12-01 10:14:01",
+    "ip": "10.180.7.3",
+    "idEjecutor": "pdr",
+    "interfaceName": "7/0/3.4085",
+    "totalSesiones": 0,
+    "sesionesEstablecidas": 0,
+    "totalPeers": 0,
+    "peerFull": 0,
+    "version": null,
+    "outfileb64": "c3Bhd2Qo=",
+    "state": "Master"
+  });
+  res.status(200)
+  res.end()
 }
 
 
 export async function validacionInterfaces(req: Request, res: Response): Promise<Response | void> {
 
-    await sleep(3000)
+  await sleep(3000)
 
-    res.json(true);
-    res.status(200)
-    res.end()
+  res.json(true);
+  res.status(200)
+  res.end()
 }
 
 
 export async function down(req: Request, res: Response): Promise<Response | void> {
-    await sleep(3000)
-    res.json(true);
-    res.status(200)
-    res.end()
+  await sleep(3000)
+  res.json(true);
+  res.status(200)
+  res.end()
 }
 
 export async function up(req: Request, res: Response): Promise<Response | void> {
-    await sleep(3000)
-    res.json(true);
-    res.status(200)
-    res.end()
+  await sleep(3000)
+  res.json(true);
+  res.status(200)
+  res.end()
 }
 
 export async function DOWN2COMMIT(req: Request, res: Response): Promise<Response | void> {
-    await sleep(3000)
-    res.json({
-        "respuestaBoolean": true,
-        "codigoRespuesta": "000",
-        "descripcionRespuesta": "La interfaz 100GE7/1/1 esta en estado DOWN",
-        "mensajeServicio": "No_Message_Found",
-        "fechaSolicitud": "2021-12-01 10:16:35",
-        "fechaRespuesta": "2021-12-01 10:16:46",
-        "ip": "10.180.7.1",
-        "idEjecutor": "pdr",
-        "interfaceName": "100GE7/1/1",
-        "totalSesiones": 0,
-        "sesionesEstablecidas": 0,
-        "totalPeers": 0,
-        "peerFull": 0,
-        "version": null,
-        "outfileb64": "IyBXZCg==",
-        "state": null
-      });
-    res.status(200)
-    res.end()
+  await sleep(3000)
+  res.json({
+    "respuestaBoolean": true,
+    "codigoRespuesta": "000",
+    "descripcionRespuesta": "La interfaz 100GE7/1/1 esta en estado DOWN",
+    "mensajeServicio": "No_Message_Found",
+    "fechaSolicitud": "2021-12-01 10:16:35",
+    "fechaRespuesta": "2021-12-01 10:16:46",
+    "ip": "10.180.7.1",
+    "idEjecutor": "pdr",
+    "interfaceName": "100GE7/1/1",
+    "totalSesiones": 0,
+    "sesionesEstablecidas": 0,
+    "totalPeers": 0,
+    "peerFull": 0,
+    "version": null,
+    "outfileb64": "IyBXZCg==",
+    "state": null
+  });
+  res.status(200)
+  res.end()
 }
 
 
 export async function UP2COMMIT(req: Request, res: Response): Promise<Response | void> {
-    await sleep(3000)
-    res.json({
-        "respuestaBoolean": true,
-        "codigoRespuesta": "000",
-        "descripcionRespuesta": "La interfaz 100GE7/1/1 esta en estado UP",
-        "mensajeServicio": "No_Message_Found",
-        "fechaSolicitud": "2021-12-01 10:16:35",
-        "fechaRespuesta": "2021-12-01 10:16:46",
-        "ip": "10.180.7.1",
-        "idEjecutor": "pdr",
-        "interfaceName": "100GE7/1/1",
-        "totalSesiones": 0,
-        "sesionesEstablecidas": 0,
-        "totalPeers": 0,
-        "peerFull": 0,
-        "version": null,
-        "outfileb64": "IyBXZCg==",
-        "state": null
-      });
-    res.status(200)
-    res.end()
+  await sleep(3000)
+  res.json({
+    "respuestaBoolean": true,
+    "codigoRespuesta": "000",
+    "descripcionRespuesta": "La interfaz 100GE7/1/1 esta en estado UP",
+    "mensajeServicio": "No_Message_Found",
+    "fechaSolicitud": "2021-12-01 10:16:35",
+    "fechaRespuesta": "2021-12-01 10:16:46",
+    "ip": "10.180.7.1",
+    "idEjecutor": "pdr",
+    "interfaceName": "100GE7/1/1",
+    "totalSesiones": 0,
+    "sesionesEstablecidas": 0,
+    "totalPeers": 0,
+    "peerFull": 0,
+    "version": null,
+    "outfileb64": "IyBXZCg==",
+    "state": null
+  });
+  res.status(200)
+  res.end()
+}
+
+export async function obtenerModeloVersionOLT(req: Request, res: Response): Promise<Response | void> {
+  await sleep(1000)
+  const random = Math.random() * (10 - 1) + 1;
+  console.log("random: ", random)
+
+  const valueModel = (random % 2 == 0) ? "V800R012C10SPC300": "MA5800V100R019C10"
+
+
+  res.json({
+    "ips": [
+      {
+        "descripcion": "LW_140639_CHIHUAHUA-CARRANZA",
+        "ip": "LW_140639_CHIHUAHUA-CARRANZA",
+        "marca": "10.59.244.76",
+        "version": "HUAWEI",
+        "model": valueModel,
+        "outfileb64": null
+      }
+    ]
+  });
+  res.status(200)
+  res.end()
 }
--- src/routes/demo.route.ts
+++ src/routes/demo.route.ts
@@ -3,53 +3,36 @@ import { login } from '../controllers/login.controller'
 import { getCredentials, userLog, consultarRFC, evidences, changestatus,
     getIpRfc, ejecutaProtocolos,
     token, getPerformance, datosOSPFv2, validacionLLDP, validateVRRP, validacionInterfaces,
-    down, UP2COMMIT, DOWN2COMMIT, up, validacionacceso
+    down, UP2COMMIT, DOWN2COMMIT, up, validacionacceso, obtenerModeloVersionOLT
 } from '../controllers/proxy.controller'
 
 const router = Router();
 
 router.route('/rings-api/rings/getPerformance').get(getPerformance);
-
 router.route('/rings-api/rings/token').get(token);
-
 router.route('/rings-api/rings/getCredentials').get(getCredentials);
-
 router.route('/rings-api/admin/userLog').get(userLog);
-
 router.route('/pdrservice/ticketAPI/consultarRFC').get(consultarRFC);
-
 router.route('/rings-api/rings/evidences').get(evidences);
-
 router.route('/rings-api/rings/changestatus').get(changestatus);
-
 router.route('/rings-api/rings/getIpRfc').get(getIpRfc);
-
 router.route('/pdrservice/proxyStep/validacionacceso').post(validacionacceso);
-
 router.route('/pdrservice/proxyStep/datosOSPFv2').post(datosOSPFv2);
-
 router.route('/pdrservice/proxyStep/datosBgpSumm').get(ejecutaProtocolos);
-
 router.route('/pdrservice/proxyStep/datosBgpAllSumm').get(ejecutaProtocolos);
-
 router.route('/pdrservice/proxyStep/validacionLLDP').get(validacionLLDP);
 router.route('/pdrservice/proxyModels/validateVRRP').post(validateVRRP);
-
-
 router.route('/pdrservice/proxyStep/servicioVoz').get(ejecutaProtocolos);
 router.route('/pdrservice/proxyStep/servicioInternet').get(ejecutaProtocolos);
 router.route('/pdrservice/proxyStep/validacionMPLS').get(ejecutaProtocolos);
-
 router.route('/pdrservice/proxyStep/validacionInterfaces').get(validacionInterfaces);
-
-
 router.route('/pdrservice/proxyStep/down').get(down);
-
 router.route('/pdrservice/proxyStep/DOWN2COMMIT').get(DOWN2COMMIT);
-
-
 router.route('/pdrservice/proxyStep/UP2COMMIT').get(UP2COMMIT);
 router.route('/pdrservice/proxyStep/up').get(up);
+
+router.route('/pdrservice/anillos/obtenerModeloVersionOLT').post(obtenerModeloVersionOLT);
+
 
 
 
